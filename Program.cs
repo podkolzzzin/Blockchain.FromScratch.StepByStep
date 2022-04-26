bce209b21242397bc672cc9528a8b9f72ebbd765
Andrii Podkolzin <podkolzzzin1996@gmail.com>
2022-04-27 00:23:58 +0300

    Add BlockchainBuilder and sample for it

--- Program.cs
+++ Program.cs
@@ -1,4 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-new Sample1.SampleBlockchainAntiCorruption().DoSample();
+var builder = new BlockchainBuilder(new CRC32Hash(), null);
+foreach (var i in Enumerable.Range(0, 10))
+{
+    Console.WriteLine(builder.AddBlock(i.ToString()));
+}
 
+record BlockchainBlock(string ParentHash, string Data);
+
+class BlockchainBuilder
+{
+    private readonly IHashFunction _hashFunction;
+    private string? _tail = null;
+
+    public BlockchainBuilder(IHashFunction hashFunction, string? tail)
+    {
+        _hashFunction = hashFunction;
+        _tail = tail;
+    }
+
+    public BlockchainBlock AddBlock(string data)
+    {
+        var block = new BlockchainBlock(_tail, data);
+        _tail = _hashFunction.GetHash(block.ParentHash + block.Data);
+        return block;
+    }
+}
