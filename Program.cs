5c8c30ee04007adf3b67d24079e8f5a019b5a2c7
Andrii Podkolzin <podkolzzzin1996@gmail.com>
2022-04-27 00:17:05 +0300

    Add sample that shows anti-corruption

--- Program.cs
+++ Program.cs
@@ -1,3 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine(new CRC32Hash().GetHash("Hello, World!"));
+new SampleBlockchainAntiCorruption().DoSample();
+
+record Block(string Hash, string Data);
+
+class SampleBlockchainAntiCorruption
+{
+    public void DoSample()
+    {
+        var hashFunction = new CRC32Hash();
+        var data = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        string parentHash = null!;
+        foreach (var item in data)
+        {
+            var block = new Block(parentHash, item.ToString());
+            Console.WriteLine(block);
+            parentHash = hashFunction.GetHash(block.Hash + block.Data);
+        }
+
+        Console.WriteLine("================================");
+        
+        data[2] = 50;
+        parentHash = null!;
+        foreach (var item in data)
+        {
+            var block = new Block(parentHash, item.ToString());
+            Console.WriteLine(block);
+            parentHash = hashFunction.GetHash(block.Hash + block.Data);
+        }
+    }
+}
