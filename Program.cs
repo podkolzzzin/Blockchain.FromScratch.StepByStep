083a296fc720bae876b1adef8ae529f3627fafb1
Andrii Podkolzin <podkolzzzin1996@gmail.com>
2022-04-27 00:18:40 +0300

    Move first sample to a separate file

--- Program.cs
+++ Program.cs
@@ -1,32 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-new SampleBlockchainAntiCorruption().DoSample();
+new Sample1.SampleBlockchainAntiCorruption().DoSample();
 
-record Block(string Hash, string Data);
-
-class SampleBlockchainAntiCorruption
-{
-    public void DoSample()
-    {
-        var hashFunction = new CRC32Hash();
-        var data = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        string parentHash = null!;
-        foreach (var item in data)
-        {
-            var block = new Block(parentHash, item.ToString());
-            Console.WriteLine(block);
-            parentHash = hashFunction.GetHash(block.Hash + block.Data);
-        }
-
-        Console.WriteLine("================================");
-        
-        data[2] = 50;
-        parentHash = null!;
-        foreach (var item in data)
-        {
-            var block = new Block(parentHash, item.ToString());
-            Console.WriteLine(block);
-            parentHash = hashFunction.GetHash(block.Hash + block.Data);
-        }
-    }
-}
---
+++ Sample1.cs
@@ -0,0 +1,31 @@
+﻿namespace Sample1
+{
+    record Block(string Hash, string Data);
+
+    class SampleBlockchainAntiCorruption
+    {
+        public void DoSample()
+        {
+            var hashFunction = new CRC32Hash();
+            var data = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            string parentHash = null!;
+            foreach (var item in data)
+            {
+                var block = new Block(parentHash, item.ToString());
+                Console.WriteLine(block);
+                parentHash = hashFunction.GetHash(block.Hash + block.Data);
+            }
+
+            Console.WriteLine("================================");
+        
+            data[2] = 50;
+            parentHash = null!;
+            foreach (var item in data)
+            {
+                var block = new Block(parentHash, item.ToString());
+                Console.WriteLine(block);
+                parentHash = hashFunction.GetHash(block.Hash + block.Data);
+            }
+        }
+    }
+}
