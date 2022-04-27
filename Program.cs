9cfe11fc1d629b65a48235dcec415108f581c5ab
Andrii Podkolzin <podkolzzzin1996@gmail.com>
2022-04-27 12:39:46 +0300

    Add TypedBlockchain and sample PopulationCensusApp

--- Program.cs
+++ Program.cs
@@ -1,23 +1,67 @@
-﻿var rsaEncryptor = new RSAEncryptor();
-var keyPair = rsaEncryptor.GenerateKeys();
-Console.WriteLine(keyPair);
+﻿using System.Collections;
+using System.Text.Json;
 
-var someData = "Hello World";
-var signedBlock = rsaEncryptor.Sign(keyPair.PrivateKey, someData);
-if (rsaEncryptor.VerifySign(keyPair.PublicKey, someData, signedBlock))
-    Console.WriteLine("Signed correctly");
-else
-    Console.Error.WriteLine("Signed incorrectly");
+var hashFunction = new CRC32Hash();
+var app = new PopulationCensusApp(new TypedBlockchain<Person>(new Blockchain(hashFunction), hashFunction));
+app.AddPerson(new Person("Iggy", "Pop"));
+app.AddPerson(new Person("Mick", "Jagger"));
 
-if (rsaEncryptor.VerifySign(keyPair.PublicKey, someData, signedBlock.Replace('A', 'B')))
-    Console.WriteLine("Signed correctly");
-else
-    Console.Error.WriteLine("Signed incorrectly");
+app.PrintAll();
 
+record Person(string Name, string Surname);
+class PopulationCensusApp
+{
+    private readonly ITypedBlockchain<Person> _registry;
 
-var anotherPair = rsaEncryptor.GenerateKeys();
-if (rsaEncryptor.VerifySign(anotherPair.PublicKey, someData, signedBlock))
-    Console.WriteLine("Signed correctly");
-else
-    Console.Error.WriteLine("Signed incorrectly");
+    public PopulationCensusApp(ITypedBlockchain<Person> registry)
+    {
+        _registry = registry;
+    }
 
+    public void AddPerson(Person person)
+    {
+        _registry.AddBlock(person);
+    }
+
+    public void PrintAll()
+    {
+        foreach (var personBlock in _registry)
+        {
+            Console.WriteLine(personBlock);
+        }
+    }
+}
+
+record struct Block<T>(string Hash, string ParentHash, string Raw, T Data);
+
+interface ITypedBlockchain<T> : IEnumerable<Block<T>>
+{
+    void AddBlock(T data);
+}
+
+class TypedBlockchain<T> : ITypedBlockchain<T>
+{
+    private readonly IBlockchain _blockchain;
+    private readonly BlockchainBuilder _builder;
+    
+    public TypedBlockchain(IBlockchain blockchain, IHashFunction hashFunction)
+    {
+        _blockchain = blockchain;
+        _builder = new BlockchainBuilder(hashFunction, _blockchain.LastOrDefault()?.Hash);
+    }
+
+    public void AddBlock(T data)
+    {
+        var dataStr = JsonSerializer.Serialize(data);
+        var block = _builder.BuildBlock(dataStr);
+        _blockchain.AddBlock(block);
+    }
+
+    public IEnumerator<Block<T>> GetEnumerator()
+    {
+        return _blockchain.Select(x => new Block<T>(x.Hash, x.ParentHash, x.Data, JsonSerializer.Deserialize<T>(x.Data)!))
+            .GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
---
+++ SampleRSAClass.cs
@@ -0,0 +1,30 @@
+﻿namespace SampleRSA;
+
+public class SampleRSAClass
+{
+    public void DoSample()
+    {
+        var rsaEncryptor = new RSAEncryptor();
+        var keyPair = rsaEncryptor.GenerateKeys();
+        Console.WriteLine(keyPair);
+
+        var someData = "Hello World";
+        var signedBlock = rsaEncryptor.Sign(keyPair.PrivateKey, someData);
+        if (rsaEncryptor.VerifySign(keyPair.PublicKey, someData, signedBlock))
+            Console.WriteLine("Signed correctly");
+        else
+            Console.Error.WriteLine("Signed incorrectly");
+
+        if (rsaEncryptor.VerifySign(keyPair.PublicKey, someData, signedBlock.Replace('A', 'B')))
+            Console.WriteLine("Signed correctly");
+        else
+            Console.Error.WriteLine("Signed incorrectly");
+
+
+        var anotherPair = rsaEncryptor.GenerateKeys();
+        if (rsaEncryptor.VerifySign(anotherPair.PublicKey, someData, signedBlock))
+            Console.WriteLine("Signed correctly");
+        else
+            Console.Error.WriteLine("Signed incorrectly");
+    }
+}
