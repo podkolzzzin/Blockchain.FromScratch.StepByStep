5af1e82cb26c92380d3a03252b11b738671f9087
Andrii Podkolzin <podkolzzzin1996@gmail.com>
2022-04-27 12:49:52 +0300

    Implement rule mechanism & Person Validation

--- Program.cs
+++ Program.cs
@@ -1,7 +1,28 @@
 ﻿var hashFunction = new CRC32Hash();
-var app = new PopulationCensusApp(new TypedBlockchain<Person>(new Blockchain(hashFunction), hashFunction));
+var app = new PopulationCensusApp(
+    new TypedBlockchain<Person>(
+        new Blockchain(hashFunction),
+        hashFunction,
+        new DuplicationRule()
+        )
+    );
+
 app.AddPerson(new Person("Iggy", "Pop"));
 app.AddPerson(new Person("Mick", "Jagger"));
 app.AddPerson(new Person("Iggy", "Pop"));
 
 app.PrintAll();
+
+interface IRule<T>
+{
+    void Execute(IEnumerable<Block<T>> builtBlocks, T newData);
+}
+
+class DuplicationRule : IRule<Person>
+{
+    public void Execute(IEnumerable<Block<Person>> builtBlocks, Person newData)
+    {
+        if (builtBlocks.Any(x => x.Data.Equals(newData)))
+            throw new ApplicationException($"Person {newData} is already presented in blockchain.");
+    }
+}
--- TypedBlockchain.cs
+++ TypedBlockchain.cs
@@ -11,15 +11,22 @@ class TypedBlockchain<T> : ITypedBlockchain<T>
 {
     private readonly IBlockchain _blockchain;
     private readonly BlockchainBuilder _builder;
-    
-    public TypedBlockchain(IBlockchain blockchain, IHashFunction hashFunction)
+    private readonly IRule<T>[] _businessRules;
+
+    public TypedBlockchain(IBlockchain blockchain, IHashFunction hashFunction, params IRule<T>[] businessRules)
     {
         _blockchain = blockchain;
         _builder = new BlockchainBuilder(hashFunction, _blockchain.LastOrDefault()?.Hash);
+        _businessRules = businessRules;
     }
 
     public void AddBlock(T data)
     {
+        foreach (var rule in _businessRules)
+        {
+            rule.Execute(this, data);
+        }
+        
         var dataStr = JsonSerializer.Serialize(data);
         var block = _builder.BuildBlock(dataStr);
         _blockchain.AddBlock(block);
