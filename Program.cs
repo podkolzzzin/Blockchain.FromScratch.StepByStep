f092c34bade6722b4448ee713e44b8f9b34568a1
Andrii Podkolzin <podkolzzzin1996@gmail.com>
2022-04-27 13:29:54 +0300

    Implement CoinApplicationApp

--- Program.cs
+++ Program.cs
@@ -15,10 +15,68 @@ app.AddPerson(new Person("Iggy", "Pop"));
 
 app.PrintAll();
 
+record Transaction(string From, string To, long Amount);
+
+record TransactionBlock(Transaction Data, string Sign) : ISigned<Transaction>
+{
+    public string PublicKey => Data.From;
+}
+
+class AmountIsAvailableRule : IRule<TransactionBlock>
+{
+    public void Execute(IEnumerable<Block<TransactionBlock>> builtBlocks, TransactionBlock newData)
+    {
+        long balance = 0;
+        var from = newData.Data.From;
+        foreach (var block in builtBlocks)
+        {
+            var signedTransaction = block.Data;
+            if (signedTransaction.Data.From == from)
+                balance -= signedTransaction.Data.Amount;
+            else
+                balance += signedTransaction.Data.Amount;
+        }
+
+        if (balance < newData.Data.Amount)
+            throw new ApplicationException(
+                $"User {newData.PublicKey} does not have {newData.Data.Amount} coins. It has only {balance} coins.");
+    }
+}
+
+class CoinBlockchainApp
+{
+    private readonly ITypedBlockchain<TransactionBlock> _blockchain;
+    private readonly IEncryptor _encryptor;
+
+    public CoinBlockchainApp()
+    {
+        var hashFunction = new CRC32Hash();
+        var lowLevelBlockchain = new Blockchain(hashFunction);
+        _encryptor = new RSAEncryptor();
+        _blockchain = new TypedBlockchain<TransactionBlock>(lowLevelBlockchain, hashFunction,
+            new SignCheckRule<TransactionBlock, Transaction>(_encryptor),
+            new AmountIsAvailableRule());
+    }
+
+    public void AcceptTransaction(TransactionBlock transactionBlock)
+    {
+        _blockchain.AddBlock(transactionBlock);
+    }
+
+    public void PerformTransaction(KeyPair fromKeys, string to, long amount)
+    {
+        var transaction = new Transaction(fromKeys.PublicKey, to, amount);
+        var transactionString = JsonSerializer.Serialize(transaction);
+        var sign = _encryptor.Sign(transactionString, fromKeys.PrivateKey);
+        var block = new TransactionBlock(transaction, sign);
+        AcceptTransaction(block);
+    }
+}
+
 interface ISigned<T>
 {
-    public string Sign { get; set; }
-    public string PublicKey { get; set; }
+    public string Sign { get; }
+    public string PublicKey { get; }
     public T Data { get; }
 }
 
